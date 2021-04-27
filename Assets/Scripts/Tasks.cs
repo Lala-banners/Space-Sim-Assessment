1ea5b0646233ed9f55ea47c52d3beaf35ec21d3f
Lala-banners <lala.l.banno@hotmail.com>
2021-04-27 16:15:45 +1000

    Design for pirate ship and player imported

---
+++ Assets/Scripts/Tasks.cs
@@ -0,0 +1,39 @@
+public class Tasks 
+{
+    /* CRITERIA
+     * Get models from Resource.Load blah blah blah
+     * In game UI eg speed measure/HUD
+     * Pooling system eg bullets or asteroids
+     * Space mining and combat Raycasting x2 mining or rangefinder
+     * Your game must also use shaders that manipulates textures
+     * In world UI
+     * Exception handling - if Andrew messes with code, no errors
+     * Log errors to file
+     * Level of detail for meshes - eg closer to asteroids, becomes more detailed
+     * Caustics and refraction shaders - caustics = sunflare heat? or half grass/ice planet where ice crystals refracts on grass?
+     * x2 screenshots of breakpoints and physics debugger
+     * Shaders on surface of objects
+     * 
+     * Change Settings Menu:
+     * Resolution
+     * Colour depth buffer
+     * Anti-aliasing
+     * point, bilinear, trilinear buffering
+     * frame rate limiter
+     * 
+     * Print DOXYGEN 
+     */
+
+    /* Game ideas
+     * Story eg: player's ship gets blown up by space pirates and is left alone floating through space. Find the pirates ship and blow it up.
+     * Travel freely through space with epic jetpack
+     * Cute helper bot (maybe kill later for emotion)
+     */
+
+    /* MECHANICS
+     * Robo Fly pulls Frog along with robo thruster wings
+     * Mouse movement 
+     * Left Shift to give speed boost - also apply boost particle/shader
+     * 
+     */
+}
