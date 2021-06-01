63e67d9f69c29a6a910acaffebf5439936bf9475
Lala-banners <lala.l.banno@hotmail.com>
2021-06-01 16:17:16 +1000

    Bullets are brighter

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,13 +10,4 @@ public class Bullet : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.up * speed;
     }
-
-    private void FixedUpdate() {
-        //Shoot bullet
-        //float moveH = Input.GetAxis("Horizontal");
-        //float moveV = Input.GetAxis("Vertical");
-
-        //Vector3 moveBullet = new Vector3(moveH, 0f, moveV);
-        
-    }
 }
